7b690f40b2c48f2a8971ad3d59c75b4af7363215
songhyun Eon <thgdus79@naver.com>
2024-05-09 15:19:38 +0900

    플레이어 백그라운드 통로 넘어가기 구현

--- src/main/BackgroundPlayerService.java
+++ src/main/BackgroundPlayerService.java
@@ -63,6 +63,20 @@ public class BackgroundPlayerService implements Runnable {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				
+				
+				// 반대쪽 통로로 빠져나오기
+				while(player.getState()==0) {
+					if(player.getX()<20 && player.getY()>330){
+						player.setX(700);
+						player.setLocation(player.getX(),player.getY());
+					} else if(player.getX()>700 && player.getY()>330){
+						player.setX(20);
+						player.setLocation(player.getX(),player.getY());
+					} else {
+						System.out.print("");
+					}
+				}
 
 			}
 
