1cc5d0f5f097e8849c22da520447c4e6b08fa43e
LOSTANA <jyj970602@naver.com>
2024-05-16 12:32:12 +0900

    시작 일시 화면 기능추가

--- src/main/PauseGame.java
+++ src/main/PauseGame.java
@@ -5,22 +5,19 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
-
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-import main.Components.Enemy1;
 
 public class PauseGame extends JFrame {
 
 	private DrawLogo drawLogo;
 	private Maingame stage;
-	private Enemy1 enemy1;
 
 	public PauseGame(Maingame stage) {
 		this.stage = stage;
@@ -125,13 +122,80 @@ public class PauseGame extends JFrame {
 						stage.getEnemy3().down();
 					}
 				}
-//				stage.getPlayer().setLeft(true);
-//				stage.getPlayer().setRight(true);
-//				stage.getPlayer().setUp(true);
-//				stage.getPlayer().setDown(true);
 				dispose();
 			}
 
+		});
+
+		this.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_SPACE:
+
+					if (stage.getEnemy().getX() <= 415 && stage.getEnemy().getX() >= 291
+							&& stage.getEnemy().getY() >= 412 && stage.getEnemy().getY() <= 468) {
+						stage.getEnemy().enemyStart();
+					} else {
+
+						if (stage.getEnemy().getDirection() == 0) {
+							stage.getEnemy().left();
+						} else if (stage.getEnemy().getDirection() == 1) {
+							stage.getEnemy().right();
+						} else if (stage.getEnemy().getDirection() == 2) {
+							stage.getEnemy().up();
+						} else {
+							stage.getEnemy().down();
+						}
+					}
+					if (stage.getEnemy2().getX() <= 415 && stage.getEnemy2().getX() >= 291
+							&& stage.getEnemy2().getY() >= 412 && stage.getEnemy2().getY() <= 468) {
+						stage.getEnemy2().enemyStart();
+					} else {
+						if (stage.getEnemy2().getDirection() == 0) {
+							stage.getEnemy2().left();
+						} else if (stage.getEnemy2().getDirection() == 1) {
+							stage.getEnemy2().right();
+						} else if (stage.getEnemy2().getDirection() == 2) {
+							stage.getEnemy2().up();
+						} else {
+							stage.getEnemy2().down();
+						}
+					}
+
+					if (stage.getEnemy3().getX() <= 415 && stage.getEnemy3().getX() >= 291
+							&& stage.getEnemy3().getY() >= 412 && stage.getEnemy3().getY() <= 468) {
+						stage.getEnemy3().enemyStart();
+					} else {
+						if (stage.getEnemy3().getDirection() == 0) {
+							stage.getEnemy3().left();
+						} else if (stage.getEnemy3().getDirection() == 1) {
+							stage.getEnemy3().right();
+						} else if (stage.getEnemy3().getDirection() == 2) {
+							stage.getEnemy3().up();
+						} else {
+							stage.getEnemy3().down();
+						}
+					}
+					dispose();
+					break;
+
+				case KeyEvent.VK_ESCAPE:
+
+					System.exit(0);
+					break;
+
+				}
+			}
 		});
 	}
 
@@ -154,6 +218,7 @@ public class PauseGame extends JFrame {
 			g.drawString("게임을 재시작하려면 클릭해주세요!", 140, 450);
 			g.setFont(new Font("DungGeunMo", Font.BOLD, 25));
 			g.drawString("Pause Game!", 300, 500);
+			g.drawString("(ESC 클릭시 게임 종료)", 300, 520);
 
 		}
 
