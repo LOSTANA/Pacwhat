fcfe9e448a91455a4eb21671594c692f44c7953f
songhyun Eon <greenery3699@gmail.com>
2024-05-20 16:30:26 +0900

    커밋커밋

--- src/main/Service/BackgroundPlayerService2.java
+++ src/main/Service/BackgroundPlayerService2.java
@@ -37,22 +37,6 @@ public class BackgroundPlayerService2 implements Runnable {
 		}
 	}
 
-	public boolean isStrong() {
-		return strong;
-	}
-
-	public void setStrong(boolean strong) {
-		this.strong = strong;
-	}
-
-	public boolean isFlag() {
-		return flag;
-	}
-
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
-
 	@Override
 	public void run() {
 		this.player = stage.getPlayer();
@@ -62,11 +46,13 @@ public class BackgroundPlayerService2 implements Runnable {
 		this.enemy3 = stage.getEnemy3();
 		// 플레이어 state가 1일 때(=살아 있을 때)
 		// 플레이어-에너미 좌표를 확인 후, 중첩 시 공격 처리
-		System.out.println("백그2 시작");
+		
+		
 		while (flag) {
-			System.out.println("1");
+			// 플레이어가 과일(무적 상태 아이템)을 먹지 않았을 때
 			if (player.getState() != 2) {
 				if (player.getState() == 1) {
+					// 에너미1-좌표 감지
 					if (Math.abs((player.getX() + 28 / 2) - (enemy1.getX() + 28 / 2)) < (28 / 2 + 28 / 2)
 							&& Math.abs((player.getY() + 28 / 2) - (enemy1.getY() + 28 / 2)) < (28 / 2 + 28 / 2)) {
 						player.isBeAttacked1();
@@ -81,56 +67,38 @@ public class BackgroundPlayerService2 implements Runnable {
 					}
 				}
 
+				// 플레이어가 과일(무적 상태 아이템)을 먹었을 때
 				else if (player.getState() == 2) {
 
-					System.out.println("if 통과");
-					strong = false;
-
-					for (int i = 0; i < 1000; i++) {
+					for (int i = 0; i < 600; i++) {
 						if (player.strong == true) {
 							i = 0;
 							player.strong = false;
 						}
-						System.out.println("while 시작");
+						
 						// 에너미 1 감지
 						if (Math.abs((player.getX() + 28 / 2) - (enemy1.getX() + 28 / 2)) < (28 / 2 + 28 / 2)
 								&& Math.abs((player.getY() + 28 / 2) - (enemy1.getY() + 28 / 2)) < (28 / 2 + 28 / 2)) {
-							System.out.println("적 공격 시작1");
-							// System.out.println(player.getEatedCount());
-							// System.out.println(player.getScore());
 							player.setEatedCount(player.getEatedCount() + 100);
 
 							// 에너미 2 감지
 						} else if (Math.abs((player.getX() + 28 / 2) - (enemy2.getX() + 28 / 2)) < (28 / 2 + 28 / 2)
 								&& Math.abs((player.getY() + 28 / 2) - (enemy2.getY() + 28 / 2)) < (28 / 2 + 28 / 2)) {
-							System.out.println("적 공격 시작2");
-							// System.out.println(player.getEatedCount());
-							// System.out.println(player.getScore());
 							player.setEatedCount(player.getEatedCount() + 100);
 
 							// 에너미 3 감지
 						} else if (Math.abs((player.getX() + 28 / 2) - (enemy3.getX() + 28 / 2)) < (28 / 2 + 28 / 2)
 								&& Math.abs((player.getY() + 28 / 2) - (enemy3.getY() + 28 / 2)) < (28 / 2 + 28 / 2)) {
-							System.out.println("적 공격 시작3");
-							// System.out.println(player.getEatedCount());
-							// System.out.println(player.getScore());
 							player.setEatedCount(player.getEatedCount() + 100);
-
-						} else {
-							System.out.println("if 통과 못함");
-						}
-						// System.out.println(i+"번째");
+							}
 
 						try {
 							Thread.sleep(10);
 						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-						System.out.println(i + "초 경과");
 					}
 					// 무적상태 (6초 후) 원래 상태로 복귀
-					System.out.println("set state==1");
 					player.setState(1);
 				}
 
